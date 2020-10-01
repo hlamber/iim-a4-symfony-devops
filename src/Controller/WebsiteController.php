ecf3857d00b2d7acb3d6ac7cc77e6810043a3f3f
Hugo Lambert <lamberthugo3@gmail.com>
2020-10-01 14:20:13 +0200

    commit 8

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -10,8 +10,14 @@ class WebsiteController extends AbstractController
     /**
      * @Route("/", name="website")
      */
-    public function index()
+    public function index(PostRepository $postRepository)
     {
+        $posts = $postRepository->findAll();
+        foreach ($posts as $post){
+
+        }
+        print_r($posts)
+        
         return $this->render('website/index.html.twig', [
             'controller_name' => 'WebsiteController',
         ]);
